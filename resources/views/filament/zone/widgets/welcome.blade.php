908eebd666616f1f8cc0445e2944963245aff67b
Faysal Ahamed <devfaysal@gmail.com>
2023-10-23 12:58:28 +0600

    wip

--- resources/views/filament/zone/widgets/welcome.blade.php
+++ resources/views/filament/zone/widgets/welcome.blade.php
@@ -1,60 +1,44 @@
-<x-filament-widgets::widget>
+@php
+    $user = filament()->auth()->user();
+@endphp
+
+<x-filament-widgets::widget class="fi-account-widget">
     <x-filament::section>
-        <div class="fi-section-content p-6">
-            <div class="flex items-center gap-x-3">
-            <div style="background-image: url('https://ui-avatars.com/api/?name=B&amp;color=FFFFFF&amp;background=09090b');" class="fi-avatar bg-cover bg-center h-10 w-10 fi-user-avatar rounded-full"></div>
+        <div class="flex items-center gap-x-3">
+            <x-filament-panels::avatar.user size="lg" :user="$user" />
+
             <div class="flex-1">
                 <a href="/zone/profile">
-                    <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
-                        Welcome
+                    <h2
+                        class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
+                    >
+                        {{ __('filament-panels::widgets/account-widget.welcome', ['app' => config('app.name')]) }}
                     </h2>
 
                     <p class="text-sm text-gray-500 dark:text-gray-400">
-                        Banglabandha-Hili
+                        {{ filament()->getUserName($user) }}
                     </p>
                 </a>
             </div>
-            
 
-            <form action="http://office.surovigroup.net:96/zone/logout" method="post" class="my-auto -me-2.5 sm:me-0">
-                <input type="hidden" name="_token" value="2FE0SHFQKGNrH7PKnut7aXWgjX8iWENBru2vEHnQ" autocomplete="off">
-                <!--[if BLOCK]><![endif]-->    <button style="--c-300:var(--gray-300);--c-400:var(--gray-400);--c-500:var(--gray-500);--c-600:var(--gray-600);" class="fi-icon-btn relative flex items-center justify-center rounded-lg outline-none transition duration-75 focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-70 h-9 w-9 fi-color-gray text-gray-400 hover:text-gray-500 focus-visible:ring-primary-600 dark:text-gray-500 dark:hover:text-gray-400 dark:focus-visible:ring-primary-500 sm:hidden" title="Sign out" type="submit">
-                    <span class="sr-only">
-                Sign out
-            </span>
-        
-        <svg class="fi-icon-btn-icon h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-  <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd"></path>
-  <path fill-rule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clip-rule="evenodd"></path>
-</svg>
-        
-            </button>
- <!--[if ENDBLOCK]><![endif]-->
+            <form
+                action="{{ filament()->getLogoutUrl() }}"
+                method="post"
+                class="my-auto -me-2.5 sm:me-0"
+            >
+                @csrf
 
-<!--[if BLOCK]><![endif]-->    <button style=";" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-gray fi-btn-color-gray fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm hidden sm:inline-grid shadow-sm bg-white text-gray-950 hover:bg-gray-50 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 ring-1 ring-gray-950/10 dark:ring-white/20" type="submit" wire:loading.attr="disabled">
-        <!--[if BLOCK]><![endif]-->            <!--[if BLOCK]><![endif]-->                <svg class="fi-btn-icon h-5 w-5 text-gray-400 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-  <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd"></path>
-  <path fill-rule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clip-rule="evenodd"></path>
-</svg>             <!--[if ENDBLOCK]><![endif]-->
-
-            <!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
-
-            <!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
-         <!--[if ENDBLOCK]><![endif]-->
-
-        <span class="fi-btn-label">
-            Sign out
-        </span>
-
-        <!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
-
-        <!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
-
-        <!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
-    </button>
- <!--[if ENDBLOCK]><![endif]-->
+                <x-filament::button
+                    color="gray"
+                    icon="heroicon-m-arrow-left-on-rectangle"
+                    icon-alias="panels::widgets.account.logout-button"
+                    labeled-from="sm"
+                    tag="button"
+                    type="submit"
+                >
+                    {{ __('filament-panels::widgets/account-widget.actions.logout.label') }}
+                </x-filament::button>
             </form>
-        </div>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
